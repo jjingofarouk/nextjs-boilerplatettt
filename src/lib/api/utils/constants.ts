b10a37af62e4fc1258b4f72fea94ae5a18e7bc8c
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-03 19:06:20 +0300

    Create constants.ts

---
+++ src/lib/api/utils/constants.ts
@@ -0,0 +1,6 @@
+export const CATEGORIES = [
+  { value: 'grants', label: 'Grants' },
+  { value: 'scholarships', label: 'Scholarships' },
+  { value: 'fellowships', label: 'Fellowships' },
+  { value: 'conferences', label: 'Conferences' },
+];
