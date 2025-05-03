a6a1d556192febde0144dbfc6da11d1684a7d6f3
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-03 18:42:02 +0300

    Create logout.ts

---
+++ src/app/api/auth/logout.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { signOut } from 'next-auth/react';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  await signOut({ redirect: false });
+  return res.status(200).json({ message: 'Logged out' });
+}
